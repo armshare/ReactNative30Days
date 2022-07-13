37572da73fa6ba6ee244907b1ee9e8fb9aae4910
armshare <armshare.it@gmail.com>
2022-07-13 20:23:20 +0700

    finish first week

--- App.js
+++ App.js
@@ -1,36 +1,78 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { Dimensions, StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { Dimensions, StyleSheet, Text, View } from "react-native";
+import Constants from "expo-constants";
 
-export default()=>{
-  const {width} = Dimensions.get('window');
-  return (
-    <View>
-      <Text style={{padding:10}}>row (Default)</Text>
-      <View style={{flexDirection:'row', width,height:width, backgroundColor:'whitesmoke'}} >
-        <View style={{ width:50, height:50, backgroundColor:'powderblue'}}/>
-        <View style={{ width:50, height:50, backgroundColor:'skyblue'}}/>
-        <View style={{ width:50, height:50, backgroundColor:'steelblue'}}/>
-      </View>
-      <Text style={{padding:10 , marginTop:10}}>column (Default)</Text>
-      <View style={{flexDirection:'column', width,height:width, backgroundColor:'whitesmoke'}} >
-        <View style={{ width:50, height:50, backgroundColor:'powderblue'}}/>
-        <View style={{ width:50, height:50, backgroundColor:'skyblue'}}/>
-        <View style={{ width:50, height:50, backgroundColor:'steelblue'}}/>
-      </View>
-    </View>
-  )
-}
-  // <View style={{flex: 1}}>
-  //   <View style={{flex:1, backgroundColor: 'powderblue'}}/>
-  //   <View style={{flex:2, backgroundColor: 'skyblue'}}/>
-  //   <View style={{flex:3, backgroundColor: 'steelblue'}}/>
-  // </View>
-  // <View>
-  //   <View style={{width:50, height:50, backgroundColor: 'powderblue'}}/>
-  //   <View style={{width:100, height:100, backgroundColor: 'skyblue'}}/>
-  //   <View style={{width:150, height:150, backgroundColor: 'steelblue'}}/>
-  // </View>
+export default () => (
+  <View style={styles.container}>
+    <Text style={styles.text}>
+      Example <Text style={styles.boldText}>bold text</Text> that wraps around
+    </Text>
+  </View>
+);
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: Constants.statusBarHeight,
+    backgroundColor: 'powderblue',
+    padding: 8,
+  },
+  text: {
+    //Text Styles
+    fontFamily: 'Helvetica',
+    fontSize: 24,
+    lineHeight: 32,
+    fontWeight: '300',
+    color: 'white',
+    textAlign : 'center',
+    //view style
+    backgroundColor: 'steelblue',
+    borderRadius: 12,
+    padding: 20,
+    width:200
+  },
+  boldText: {
+    fontWeight: 'bold',
+  },
+});
+/* <View>
+<Text style={{padding:10}}>flex-start</Text>
+<View style={{alignItems:'stretch', backgroundColor:'whitesmoke'}} >
+  <View style={{ width:50, height:50, backgroundColor:'powderblue'}}/>
+  <View style={{ width:50, height:50, backgroundColor:'skyblue'}}/>
+  <View style={{ width:50, height:50, backgroundColor:'steelblue'}}/>
+</View>
+<Text style={{padding:10 , marginTop:10}}>flex start</Text>
+<View style={{alignItems:'flex-start',  justifyContent:'center', backgroundColor:'whitesmoke'}} >
+  <View style={{ width:50, height:50, backgroundColor:'powderblue'}}/>
+  <View style={{ width:50, height:50, backgroundColor:'skyblue'}}/>
+  <View style={{ width:50, height:50, backgroundColor:'steelblue'}}/>
+</View>
+<Text style={{padding:10 , marginTop:10}}>center</Text>
+<View style={{alignItems:'center',  justifyContent:'center', backgroundColor:'whitesmoke'}} >
+  <View style={{ width:50, height:50, backgroundColor:'powderblue'}}/>
+  <View style={{ width:50, height:50, backgroundColor:'skyblue'}}/>
+  <View style={{ width:50, height:50, backgroundColor:'steelblue'}}/>
+</View>
+<Text style={{padding:10 , marginTop:10}}>flex end</Text>
+<View style={{alignItems:'flex-end',  justifyContent:'center', backgroundColor:'whitesmoke'}} >
+  <View style={{ width:50, height:50, backgroundColor:'powderblue'}}/>
+  <View style={{ width:50, height:50, backgroundColor:'skyblue'}}/>
+  <View style={{ width:50, height:50, backgroundColor:'steelblue'}}/>
+</View>
+</View> */
+
+// <View style={{flex: 1}}>
+//   <View style={{flex:1, backgroundColor: 'powderblue'}}/>
+//   <View style={{flex:2, backgroundColor: 'skyblue'}}/>
+//   <View style={{flex:3, backgroundColor: 'steelblue'}}/>
+// </View>
+// <View>
+//   <View style={{width:50, height:50, backgroundColor: 'powderblue'}}/>
+//   <View style={{width:100, height:100, backgroundColor: 'skyblue'}}/>
+//   <View style={{width:150, height:150, backgroundColor: 'steelblue'}}/>
+// </View>
 
 // export default()=>(
 //   <View style={styles.myStyle}>
@@ -68,8 +110,6 @@ export default()=>{
 //     backgroundColor: "blue"
 //   }
 // })
-
-
 // export default ()=>(
 //   <View style={{width:200, height:200, backgroundColor:'skyblue'}}/>
 //   )
